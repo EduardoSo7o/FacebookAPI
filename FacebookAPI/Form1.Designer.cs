9ede11e201f3d1f69abb30ac73e48f8129e0ed65
Luis Eduardo Carreón Soto <60121944+EduardoSo7o@users.noreply.github.com>
2020-11-04 22:49:24 -0600

    Agregar archivos de proyecto.

---
+++ FacebookAPI/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace FacebookAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.Get = new System.Windows.Forms.Button();
+            this.Post = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(12, 12);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(776, 26);
+            this.textBoxURL.TabIndex = 0;
+            // 
+            // Get
+            // 
+            this.Get.Location = new System.Drawing.Point(697, 45);
+            this.Get.Name = "Get";
+            this.Get.Size = new System.Drawing.Size(90, 90);
+            this.Get.TabIndex = 2;
+            this.Get.Text = "Get";
+            this.Get.UseVisualStyleBackColor = true;
+            this.Get.Click += new System.EventHandler(this.Get_Click);
+            // 
+            // Post
+            // 
+            this.Post.Location = new System.Drawing.Point(698, 141);
+            this.Post.Name = "Post";
+            this.Post.Size = new System.Drawing.Size(90, 90);
+            this.Post.TabIndex = 3;
+            this.Post.Text = "Post";
+            this.Post.UseVisualStyleBackColor = true;
+            this.Post.Click += new System.EventHandler(this.Post_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(679, 384);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Post);
+            this.Controls.Add(this.Get);
+            this.Controls.Add(this.textBoxURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button Get;
+        private System.Windows.Forms.Button Post;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ FacebookAPI/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace FacebookAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Get_Click(object sender, EventArgs e)
+        {
+            /*WebClient client = new WebClient();
+            String URI = textBoxURL.Text;
+            Stream data = client.OpenRead(URI);
+            StreamReader reader = new StreamReader(data);
+            String s = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+            listBox1.Items.Add(s);*/
+
+            try
+            {
+                String url = textBoxURL.Text;
+                String s;
+                WebRequest request = WebRequest.Create(url);
+                WebResponse response = request.GetResponse();
+                Stream data = response.GetResponseStream();
+                StreamReader reader = new StreamReader(data);
+                s = reader.ReadToEnd();
+                listBox1.Items.Add(s);
+                data.Close();
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                throw new System.Exception("error: " + ex.Message);
+            }
+
+
+
+        }
+
+        private void Post_Click(object sender, EventArgs e)
+        {
+            String data = "{\"name\":\"test\", \"salary\":\"123\",\"age\":\"23\"}";
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+            String URI = textBoxURL.Text;
+            String s;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URI);
+            request.ContentLength = dataBytes.Length;
+            request.ContentType = "application/json";
+            request.Method = "POST";
+
+            using(Stream requestBody = request.GetRequestStream())
+            {
+                requestBody.Write(dataBytes, 0, dataBytes.Length);
+            }
+
+            using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using(Stream stream = response.GetResponseStream())
+            using(StreamReader reader = new StreamReader(stream))
+            {
+                s = reader.ReadToEnd();
+                listBox1.Items.Add(s);
+            }
+
+            
+        }
+    }
+}
